f4c207acb72a09bc261195a1955d0e288b56f317
Michał Borecki <michal.borecki.mb@gmail.com>
2018-06-24 15:58:13 +0200

    "Change package, add imports"

--- src/main/java/pl/coderslab/carrental/persistence/dao/CarRepository.java
+++ src/main/java/pl/coderslab/carrental/persistence/dao/CarRepository.java
@@ -1,11 +1,13 @@
-package pl.coderslab.carrental.repository;
+package pl.coderslab.carrental.persistence.dao;
 
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import pl.coderslab.carrental.entity.Car;
+import org.springframework.stereotype.Repository;
+import pl.coderslab.carrental.persistence.model.Car;
 
+@Repository("carRepository")
 public interface CarRepository extends JpaRepository<Car, Long> {
 
 	List<Car> findByUserIsNull();
--- src/main/java/pl/coderslab/carrental/persistence/dao/RentRepository.java
+++ src/main/java/pl/coderslab/carrental/persistence/dao/RentRepository.java
@@ -1,4 +1,4 @@
-package pl.coderslab.carrental.repository;
+package pl.coderslab.carrental.persistence.dao;
 
 import java.util.List;
 
@@ -6,8 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import pl.coderslab.carrental.entity.Rent;
+import org.springframework.stereotype.Repository;
+import pl.coderslab.carrental.persistence.model.Rent;
 
+@Repository("rentRepository")
 public interface RentRepository extends JpaRepository<Rent, Long> {
 	
 	@Query("SELECT r FROM Rent r WHERE r.user.id LIKE :id AND r.end IS NOT NULL ORDER BY r.start ASC")
