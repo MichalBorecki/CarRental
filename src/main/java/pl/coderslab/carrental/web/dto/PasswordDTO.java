dbc25ad027672e60324bce361998cb7e825f3281
Michał Borecki <michal.borecki.mb@gmail.com>
2018-06-25 12:53:14 +0200

    Add file PasswordDto, reformat code in UserDto

---
+++ src/main/java/pl/coderslab/carrental/web/dto/PasswordDTO.java
@@ -0,0 +1,29 @@
+package pl.coderslab.carrental.web.dto;
+
+
+import pl.coderslab.carrental.validator.ValidPassword;
+
+public class PasswordDTO {
+
+    private String oldPassword;
+
+    @ValidPassword
+    private String newPassword;
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+}
